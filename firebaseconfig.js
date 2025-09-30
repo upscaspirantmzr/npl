98b8bc386e141f2534ef57df028f025a73b4f545
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-09-30 12:29:16 +0530

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB2xXyxbjPgNPs_nVtldHGGiYD1TKvY_Jw",
+  authDomain: "nyfcricket.firebaseapp.com",
+  projectId: "nyfcricket",
+  storageBucket: "nyfcricket.firebasestorage.app",
+  messagingSenderId: "9846999843",
+  appId: "1:9846999843:web:0397feb087e2dcd8c21a96",
+  measurementId: "G-50TCW6VTHL"
+};
